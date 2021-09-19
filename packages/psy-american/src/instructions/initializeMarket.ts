9472d69db53566fa7e9113309de5416018c8e018
Taylor123 <cjohns28@terpmail.umd.edu>
2021-09-18 17:35:16 -0700

    WIP

---
+++ packages/psy-american/src/instructions/initializeMarket.ts
@@ -0,0 +1,182 @@
+import * as anchor from "@project-serum/anchor";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import {
+  AccountMeta,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { feeAmount, FEE_OWNER_KEY } from "../fees";
+import { getOrAddAssociatedTokenAccountTx } from "../utils";
+
+/**
+ * Initialize new type of option
+ *
+ * @param program
+ * @param connection
+ * @param payer
+ * @param params
+ * @returns
+ */
+export const initializeMarket = async (
+  program: anchor.Program,
+  payer: Keypair,
+  connection: Connection,
+  {
+    expirationUnixTimestamp,
+    quoteAmountPerContract,
+    quoteMint,
+    underlyingAmountPerContract,
+    underlyingMint,
+  }: {
+    expirationUnixTimestamp: anchor.BN;
+    quoteAmountPerContract: anchor.BN;
+    quoteMint: PublicKey;
+    underlyingAmountPerContract: anchor.BN;
+    underlyingMint: PublicKey;
+  }
+): Promise<{
+  optionMarketKey: PublicKey;
+  optionMintKey: PublicKey;
+  quoteAssetPoolKey: PublicKey;
+  tx: string;
+  underlyingAssetPoolKey: PublicKey;
+  writerMintKey: PublicKey;
+}> => {
+  const textEncoder = new TextEncoder();
+
+  // generate Program Derived Address for the new option
+  const [optionMarketKey, bumpSeed] =
+    await anchor.web3.PublicKey.findProgramAddress(
+      [
+        underlyingMint.toBuffer(),
+        quoteMint.toBuffer(),
+        underlyingAmountPerContract.toBuffer("le", 8),
+        quoteAmountPerContract.toBuffer("le", 8),
+        expirationUnixTimestamp.toBuffer("le", 8),
+      ],
+      program.programId
+    );
+
+  // generate Program Derived Address for the Option Token
+  const [optionMintKey] = await anchor.web3.PublicKey.findProgramAddress(
+    [optionMarketKey.toBuffer(), textEncoder.encode("optionToken")],
+    program.programId
+  );
+  // generate Program Derived Address for the Writer Token
+  const [writerMintKey] = await anchor.web3.PublicKey.findProgramAddress(
+    [optionMarketKey.toBuffer(), textEncoder.encode("writerToken")],
+    program.programId
+  );
+
+  // generate Program Derived Address for the vault that will hold the quote asset
+  const [quoteAssetPoolKey] = await anchor.web3.PublicKey.findProgramAddress(
+    [optionMarketKey.toBuffer(), textEncoder.encode("quoteAssetPool")],
+    program.programId
+  );
+
+  // generate Program Derived Address for the vault that will hold the underlying asset
+  const [underlyingAssetPoolKey] =
+    await anchor.web3.PublicKey.findProgramAddress(
+      [optionMarketKey.toBuffer(), textEncoder.encode("underlyingAssetPool")],
+      program.programId
+    );
+
+  // Determine whether the mint/exercise fee accounts need to be initialized.
+  // Add the instructions and necessary accounts if the accounts need to
+  // be created.
+  const remainingAccounts: AccountMeta[] = [];
+  const instructions: TransactionInstruction[] = [];
+  const mintFee = feeAmount(underlyingAmountPerContract);
+  if (mintFee.gtn(0)) {
+    const mintFeeKey = await Token.getAssociatedTokenAddress(
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+      TOKEN_PROGRAM_ID,
+      underlyingMint,
+      FEE_OWNER_KEY
+    );
+    remainingAccounts.push({
+      pubkey: mintFeeKey,
+      isWritable: true,
+      isSigner: false,
+    });
+    const ix = await getOrAddAssociatedTokenAccountTx(
+      mintFeeKey,
+      new Token(connection, underlyingMint, program.programId, payer),
+      payer.publicKey,
+      FEE_OWNER_KEY
+    );
+    if (ix) {
+      instructions.push(ix);
+    }
+  }
+
+  const exerciseFee = feeAmount(quoteAmountPerContract);
+  if (exerciseFee.gtn(0)) {
+    const exerciseFeeKey = await Token.getAssociatedTokenAddress(
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+      TOKEN_PROGRAM_ID,
+      quoteMint,
+      FEE_OWNER_KEY
+    );
+    remainingAccounts.push({
+      pubkey: exerciseFeeKey,
+      isWritable: false,
+      isSigner: false,
+    });
+    const ix = await getOrAddAssociatedTokenAccountTx(
+      exerciseFeeKey,
+      new Token(connection, quoteMint, program.programId, payer),
+      payer.publicKey,
+      FEE_OWNER_KEY
+    );
+    if (ix) {
+      instructions.push(ix);
+    }
+  }
+
+  const tx = await program.rpc.initializeMarket(
+    underlyingAmountPerContract,
+    quoteAmountPerContract,
+    expirationUnixTimestamp,
+    bumpSeed,
+    {
+      accounts: {
+        authority: payer.publicKey,
+        feeOwner: FEE_OWNER_KEY,
+        optionMarket: optionMarketKey,
+        optionMint: optionMintKey,
+        quoteAssetMint: quoteMint,
+        quoteAssetPool: quoteAssetPoolKey,
+        underlyingAssetMint: underlyingMint,
+        underlyingAssetPool: underlyingAssetPoolKey,
+        writerTokenMint: writerMintKey,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        clock: SYSVAR_CLOCK_PUBKEY,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      },
+      instructions,
+      remainingAccounts,
+      signers: [payer],
+    }
+  );
+
+  return {
+    optionMarketKey,
+    optionMintKey,
+    quoteAssetPoolKey,
+    tx,
+    underlyingAssetPoolKey,
+    writerMintKey,
+  };
+};
---
+++ packages/psy-american/src/utils/getOrAddAssociatedTokenAccountTx.ts
@@ -0,0 +1,44 @@
+import { PublicKey } from "@solana/web3.js";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { FEE_OWNER_KEY } from "../fees";
+
+export const getOrAddAssociatedTokenAccountTx = async (
+  associatedAddress: PublicKey,
+  token: Token,
+  payer: PublicKey,
+  owner: PublicKey = FEE_OWNER_KEY
+) => {
+  // This is the optimum logic, considering TX fee, client-side computation,
+  // RPC roundtrips and guaranteed idempotent.
+  // Sadly we can't do this atomically;
+  try {
+    await token.getAccountInfo(associatedAddress);
+    return null;
+  } catch (err) {
+    // INVALID_ACCOUNT_OWNER can be possible if the associatedAddress has
+    // already been received some lamports (= became system accounts).
+    // Assuming program derived addressing is safe, this is the only case
+    // for the INVALID_ACCOUNT_OWNER in this code-path
+    if (
+      err.message === "Failed to find account" ||
+      err.message === "Invalid account owner"
+    ) {
+      // as this isn't atomic, it's possible others can create associated
+      // accounts meanwhile
+      return Token.createAssociatedTokenAccountInstruction(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        token.publicKey,
+        associatedAddress,
+        owner,
+        payer
+      );
+    } else {
+      throw err;
+    }
+  }
+};
