05781e4b61be1cf04da7b8d65d6b55b4e8c32996
Taylor123 <cjohns28@terpmail.umd.edu>
2021-09-19 14:30:22 -0700

    add initialize market binding

--- packages/psy-american/src/utils/getOrAddAssociatedTokenAccountTx.ts
+++ packages/psy-american/src/utils/getOrAddAssociatedTokenAccountTx.ts
@@ -1,44 +1,36 @@
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   Token,
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 import { FEE_OWNER_KEY } from "../fees";
+import { Provider } from "@project-serum/anchor";
 
 export const getOrAddAssociatedTokenAccountTx = async (
   associatedAddress: PublicKey,
-  token: Token,
-  payer: PublicKey,
+  mintKey: PublicKey,
+  provider: Provider,
   owner: PublicKey = FEE_OWNER_KEY
-) => {
+): Promise<TransactionInstruction | null> => {
   // This is the optimum logic, considering TX fee, client-side computation,
   // RPC roundtrips and guaranteed idempotent.
   // Sadly we can't do this atomically;
-  try {
-    await token.getAccountInfo(associatedAddress);
+  const accountInfo = await provider.connection.getAccountInfo(
+    associatedAddress
+  );
+  if (accountInfo) {
+    // accountInfo exists, so the associated token account has already
+    // been initialized
     return null;
-  } catch (err) {
-    // INVALID_ACCOUNT_OWNER can be possible if the associatedAddress has
-    // already been received some lamports (= became system accounts).
-    // Assuming program derived addressing is safe, this is the only case
-    // for the INVALID_ACCOUNT_OWNER in this code-path
-    if (
-      err.message === "Failed to find account" ||
-      err.message === "Invalid account owner"
-    ) {
-      // as this isn't atomic, it's possible others can create associated
-      // accounts meanwhile
-      return Token.createAssociatedTokenAccountInstruction(
-        ASSOCIATED_TOKEN_PROGRAM_ID,
-        TOKEN_PROGRAM_ID,
-        token.publicKey,
-        associatedAddress,
-        owner,
-        payer
-      );
-    } else {
-      throw err;
-    }
   }
+
+  return Token.createAssociatedTokenAccountInstruction(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    mintKey,
+    associatedAddress,
+    owner,
+    provider.wallet.publicKey
+  );
 };
