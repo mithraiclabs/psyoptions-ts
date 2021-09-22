ff7100099d620ace2a114b748cc0ef1ce1ba85fd
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-09-22 07:30:37 -0700

    update packaages for devnet testing

--- packages/psy-american/src/programIds.ts
+++ packages/psy-american/src/programIds.ts
@@ -1,7 +1,7 @@
 import { ProgramVersions } from "./types"
 
-export const PSY_AMERICAN_PROGRAM_IDS = () => ({
+export const PSY_AMERICAN_PROGRAM_IDS = {
   "GDvqQy3FkDB2wyNwgZGp5YkmRMUmWbhNNWDMYKbLSZ5N": ProgramVersions.V1,
   "AAcHauRu6eUDePN3hDDAUTbgyRq5xFbRZJCLsUdpBCJb": ProgramVersions.V1,
   "3KAqLcLAY8W7ZxGT1MJcFPDMNJKJsXaE1m9i1JPahfmH": ProgramVersions.V1_1
-})
+};
