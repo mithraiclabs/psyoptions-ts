974949ec67848f1041ae7d799477d16b0464e2d6
Taylor123 <cjohns28@terpmail.umd.edu>
2021-10-07 08:12:44 -0700

    return openOrdersKey from newOrderInstruction

--- packages/psy-american/src/serumInstructions/newOrder.ts
+++ packages/psy-american/src/serumInstructions/newOrder.ts
@@ -9,14 +9,14 @@ const textEncoder = new TextEncoder();
 
 /**
  * Create a new order proxied through the Psy American Protocol
- * 
+ *
  * @param program - Anchor Psy American program
  * @param optionMarketKey - The OptionMarket address
  * @param dexProgramId - The Serum DEX program ID
  * @param marketKey - The Serum market address
- * @param orderArguments - The Serum OrderParams 
+ * @param orderArguments - The Serum OrderParams
  * @param marketAuthorityBump - OPTIONAL: pass in the market authority bump seed
- * @returns 
+ * @returns
  */
 export const newOrderInstruction = async (
   program: Program,
@@ -24,12 +24,14 @@ export const newOrderInstruction = async (
   dexProgramId: PublicKey,
   marketKey: PublicKey,
   orderArguments: OrderParamsWithFeeRate<PublicKey>,
-  marketAuthorityBump: number|undefined = undefined,
-): Promise<Transaction> => {
+  marketAuthorityBump: number | undefined = undefined
+): Promise<{ openOrdersKey: PublicKey; tx: Transaction }> => {
   const transaction = new Transaction();
+  let _openOrdersKey = orderArguments.openOrdersAddressKey;
   let _marketAuthorityBump = marketAuthorityBump;
   if (!marketAuthorityBump) {
-    ({ marketAuthorityBump: _marketAuthorityBump } = await getMarketAndAuthorityInfo(program, optionMarketKey, dexProgramId));
+    ({ marketAuthorityBump: _marketAuthorityBump } =
+      await getMarketAndAuthorityInfo(program, optionMarketKey, dexProgramId));
   }
 
   const marketProxy = await marketLoader(
@@ -38,12 +40,12 @@ export const newOrderInstruction = async (
     _marketAuthorityBump,
     dexProgramId,
     marketKey
-  )
+  );
 
   // create OpenOrders account
-  if (!orderArguments.openOrdersAddressKey) {
+  if (!_openOrdersKey) {
     // Check that the OpenOrders account does not exist
-    const [openOrdersAddressKey, openOrdersBump] = await PublicKey.findProgramAddress(
+    [_openOrdersKey] = await PublicKey.findProgramAddress(
       [
         textEncoder.encode("open-orders"),
         dexProgramId.toBuffer(),
@@ -53,21 +55,28 @@ export const newOrderInstruction = async (
       program.programId
     );
     const accountInfo = await program.provider.connection.getAccountInfo(
-      openOrdersAddressKey,
-      'recent',
+      _openOrdersKey,
+      "recent"
     );
-    orderArguments.openOrdersAddressKey = openOrdersAddressKey;
+    orderArguments.openOrdersAddressKey = _openOrdersKey;
     if (!accountInfo) {
-      const {ix} = await initOpenOrdersInstruction(program, program.provider.wallet.publicKey, optionMarketKey, dexProgramId, marketKey, _marketAuthorityBump)
-      transaction.add(ix)
+      const { ix } = await initOpenOrdersInstruction(
+        program,
+        program.provider.wallet.publicKey,
+        optionMarketKey,
+        dexProgramId,
+        marketKey,
+        _marketAuthorityBump
+      );
+      transaction.add(ix);
     }
   }
 
   if (orderArguments.feeRate) {
-    orderArguments.price = orderArguments.price * (1 + orderArguments.feeRate)
+    orderArguments.price = orderArguments.price * (1 + orderArguments.feeRate);
   }
 
-  const ix = marketProxy.instruction.newOrderV3(orderArguments)
+  const ix = marketProxy.instruction.newOrderV3(orderArguments);
   transaction.add(ix);
-  return transaction
-}
+  return { openOrdersKey: _openOrdersKey, tx: transaction };
+};
