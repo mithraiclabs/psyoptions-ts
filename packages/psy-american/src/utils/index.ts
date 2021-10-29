eea23125badf6eb2930cf0f56a4b040d6282a114
Jason Chitla <jasonchitla@hotmail.com>
2021-10-28 23:35:47 -0400

    fix findOpenOrdersForOptionMarkets
    getMultipleAccountsInfo cannot take array larger than 100

--- packages/psy-american/src/utils/index.ts
+++ packages/psy-american/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./deriveOptionKeyFromParams";
 export * from "./getOrAddAssociatedTokenAccountTx";
 export * from "./getVaultOwnerAndNonce";
 export * from "./getOptionByKey";
+export * from "./chunkArray";
