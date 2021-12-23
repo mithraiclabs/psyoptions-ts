e509acb2db0e46a68c0aae95fd5c54e0653e3677
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-12-22 18:42:04 -0800

    add new settle funds API

--- packages/psy-american/src/serumInstructions/settleFunds.ts
+++ packages/psy-american/src/serumInstructions/settleFunds.ts
@@ -1,5 +1,8 @@
 import { Program } from "@project-serum/anchor";
+import { Market } from "@project-serum/serum";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
+import { FEE_OWNER_KEY } from "../fees";
 import { deriveMarketAuthority } from "../serumUtils";
 import { marketLoader } from "./marketLoader";
 
@@ -24,9 +27,9 @@ export const settleFundsInstruction = async (
   serumMarketKey: PublicKey,
   baseWallet: PublicKey,
   quoteWallet: PublicKey,
-  serumReferralKey: PublicKey, // TODO: This should probably be abstracted
+  serumReferralKey: PublicKey,
   openOrdersKey: PublicKey,
-  marketAuthorityBump: number|undefined,
+  marketAuthorityBump: number|undefined = undefined,
 ) => {
   let _marketAuthorityBump = marketAuthorityBump;
   if (!marketAuthorityBump) {
@@ -50,5 +53,57 @@ export const settleFundsInstruction = async (
     baseWallet,
     quoteWallet,
     serumReferralKey
+  )
+}
+/**
+ * Create a TransactionInstruction for the settleFunds instruction
+ * 
+ * Note: this API abstracts the complexity of the serumReferralKey away.
+ * 
+ * @param program - Anchor Psy American Program
+ * @param optionMarketKey - The OptionMarket address
+ * @param dexProgramId - The Serum DEX program ID
+ * @param serumMarket - The Serum market
+ * @param baseWallet - The wallet address that contains the user's base asset tokens
+ * @param quoteWallet - The wallet address that contains the user's quote asset tokens
+ * @param openOrdersKey - The open orders keys
+ * @param marketAuthorityBump - OPTIONAL: pass in the market authority bump seed
+ * @returns 
+ */
+export const settleMarketFundsInstruction = async (
+  program: Program,
+  optionMarketKey: PublicKey,
+  dexProgramId: PublicKey,
+  serumMarket: Market,
+  baseWallet: PublicKey,
+  quoteWallet: PublicKey,
+  openOrdersKey: PublicKey,
+) => {
+  // Get the associated address for a referral
+  const owner = FEE_OWNER_KEY;
+  const associatedAddress = await Token.getAssociatedTokenAddress(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    serumMarket.quoteMintAddress,
+    owner,
+  );
+  const accountInfo = await program.provider.connection.getAccountInfo(
+    associatedAddress,
+  );
+  if (!accountInfo) {
+    throw new Error(
+      `Referral account does not exist for ${serumMarket.quoteMintAddress.toString()}. Please create one.`,
+    );
+  }
+  return settleFundsInstruction(
+    program,
+    optionMarketKey,
+    dexProgramId,
+    serumMarket.address,
+    baseWallet,
+    quoteWallet,
+    associatedAddress,
+    openOrdersKey,
+    undefined
   )
 }
