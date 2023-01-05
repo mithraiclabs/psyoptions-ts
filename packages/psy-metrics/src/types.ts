d44377ba3a9151ab6396378a947a0a71d9bbc2d8
Tommy Johnson <tommy@psyoptions.io>
2023-01-05 11:17:42 -0800

    add function to pull VSR locked PSY

--- packages/psy-metrics/src/types.ts
+++ packages/psy-metrics/src/types.ts
@@ -18,3 +18,58 @@ export type MintInfo = {
   isInitialized: boolean;
   freezeAuthority: null | web3.PublicKey;
 };
+
+///////// Governance & VSR Types //////////
+interface LockupKind {
+  none: object
+  daily: object
+  monthly: object
+  cliff: object
+  constant: object
+}
+
+interface Lockup {
+  endTs: BN
+  kind: LockupKind
+  startTs: BN
+}
+
+export interface Deposit {
+  allowClawback: boolean
+  amountDepositedNative: BN
+  amountInitiallyLockedNative: BN
+  isUsed: boolean
+  lockup: Lockup
+  votingMintConfigIdx: number
+}
+
+export interface DepositWithWallet {
+  voter: web3.PublicKey
+  wallet: web3.PublicKey
+  deposit: Deposit
+}
+
+export interface Voter {
+  voterAuthority: web3.PublicKey;
+  registrar: web3.PublicKey;
+  deposits: Deposit[];
+  voter_bump: number;
+  voter_weight_record_bump: number;
+}
+
+export interface votingMint {
+  baselineVoteWeightScaledFactor: BN
+  digitShift: number
+  grantAuthority: web3.PublicKey
+  lockupSaturationSecs: BN
+  maxExtraLockupVoteWeightScaledFactor: BN
+  mint: web3.PublicKey
+}
+
+export interface Registrar {
+  governanceProgramId: web3.PublicKey
+  realm: web3.PublicKey
+  realmAuthority: web3.PublicKey
+  realmGoverningTokenMint: web3.PublicKey
+  votingMints: votingMint[]
+}
